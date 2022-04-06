913b5b9da64f8e7bb2993ea4bc2b119f442c969b
Azie Melza Pratama <aziemp66@gmail.com>
2022-04-06 12:47:35 +0700

    Starting Project

---
+++ Server/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+	if (!process.env.PORT) {
+		console.log(`Example app listening on port ${PORT}!`);
+	}
+});
