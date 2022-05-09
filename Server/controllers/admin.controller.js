87d1909c26b6ecb7a7144b3812acb638a7a13e47
Azie Melza Pratama <aziemp66@gmail.com>
2022-05-09 09:42:02 +0700

    Adding Add Station Endpoints

--- Server/controllers/admin.controller.js
+++ Server/controllers/admin.controller.js
@@ -1,8 +1,11 @@
+const addStation = (req, res, next) => {};
+
 const addDisease = (req, res, next) => {};
 
 const addSkill = (req, res, next) => {};
 
 module.exports = {
+	addStation,
 	addDisease,
 	addSkill,
 };
--- Server/router/admin.routes.js
+++ Server/router/admin.routes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const adminController = require("../controllers/admin.controller");
 
+router.post("/station", adminController.addStation);
+
 router.post("/disease", adminController.addDisease);
+
+router.post("/skill", adminController.addSkill);
 
 module.exports = router;
