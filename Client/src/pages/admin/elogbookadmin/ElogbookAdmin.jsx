392dd9ddbd7a2e21527fe1971380dc7bbe7961f2
aziemp66 <aziemp66@gmail.com>
2022-06-25 15:35:45 +0700

    Handling Request Type

--- Client/src/pages/admin/elogbookadmin/ElogbookAdmin.jsx
+++ Client/src/pages/admin/elogbookadmin/ElogbookAdmin.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFormState } from "react-hook-form";
 import styles from "./ElogbookAdmin.module.css";
 
 import axios from "axios";
@@ -10,7 +10,13 @@ const ElogbookAdmin = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+    control,
+  } = useForm({
+    defaultValues: {
+      requestType: "post",
+    },
+  });
+  const { dirtyFields } = useFormState({ control });
 
   const [stations, setStations] = useState(null);
   const [diseases, setDiseases] = useState(null);
@@ -19,8 +25,7 @@ const ElogbookAdmin = () => {
   const [hospitals, setHospitals] = useState(null);
 
   const objectType = watch("objectType");
-
-  console.log(objectType);
+  const requestType = watch("requestType");
 
   const baseUrl =
     (import.meta.env.API_URL && `${import.meta.env.API_URL}/api/admin`) ||
@@ -45,6 +50,11 @@ const ElogbookAdmin = () => {
   }, []);
 
   const ObjectRequestHandler = (restRequest, type, data) => {
+    console.log(restRequest);
+    console.log(type);
+    console.log(data);
+    if (objectType === "" || !objectType) return;
+    console.log("here");
     axios[restRequest](`${baseUrl}/${type}`, data, {
       headers: {
         "auth-token": localStorage.getItem("token"),
@@ -60,29 +70,38 @@ const ElogbookAdmin = () => {
 
   return (
     <div className={styles.container}>
+      <a href="#test">Test</a>
       <label htmlFor="objectType">Pilih Kategori</label>
       <select id="objectType" {...register("objectType")}>
+        {!dirtyFields.objectType && <option value={""}>Pilih Kategori</option>}
         <option value="station">Stase</option>
         <option value="disease">Penyakit</option>
         <option value="skill">Keterampilan</option>
         <option value="guidance">Metode Pembelajaran</option>
         <option value="hospital">Rumah Sakit</option>
       </select>
-      <div>
-        <h2>
-          Tambah, Edit atau Hapus{" "}
-          {(objectType === "station" && "Stase") ||
-            (objectType === "disease" && "Penyakit") ||
-            (objectType === "skill" && "Keterampilan") ||
-            (objectType === "guidance" && "Metode Pembelajaran") ||
-            (objectType === "hospital" && "Rumah Sakit")}
-        </h2>
-      </div>
-      <form
-        onSubmit={handleSubmit(
-          ObjectRequestHandler.bind(null, "post", objectType)
-        )}
-      ></form>
+
+      {objectType && (
+        <div>
+          <h2>
+            {(requestType === "post" && "Tambah") ||
+              (requestType === "patch" && "Edit") ||
+              (requestType === "delete" && "Hapus")}{" "}
+            {(objectType === "station" && "Stase") ||
+              (objectType === "disease" && "Penyakit") ||
+              (objectType === "skill" && "Keterampilan") ||
+              (objectType === "guidance" && "Metode Pembelajaran") ||
+              (objectType === "hospital" && "Rumah Sakit")}
+          </h2>
+          <form
+            onSubmit={handleSubmit(
+              ObjectRequestHandler.bind(null, "post", objectType)
+            )}
+          >
+            <button>submit</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
