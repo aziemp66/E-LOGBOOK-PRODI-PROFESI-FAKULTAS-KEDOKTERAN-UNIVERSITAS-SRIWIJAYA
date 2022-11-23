5365438c1719941c1992f572081f08bc76ae1f1c
Azie Melza Pratama <aziemp66@gmail.com>
2022-11-23 18:38:02 +0700

    Adding Env Example

--- Client/src/contexts/AuthContexts.jsx
+++ Client/src/contexts/AuthContexts.jsx
@@ -17,7 +17,10 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
-const BASE_URL = "http://localhost:5000/api/auth";
+const BASE_URL = (import.meta.env.VITE_API_URL &&
+      `${import.meta.env.VITE_API_URL}/api/student`) ||
+    "http://localhost:5000/api/student";
+
 
 export const AuthProvider = (props) => {
   const [userData, setUserData] = useState(null);
